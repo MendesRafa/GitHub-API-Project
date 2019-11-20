7806d80eec357c7de65ca4436b0c84fc268e86b5
MendesRafa <rafaman089@outlook.com>
2019-11-20 10:09:55 +0000

    Request to get repos by topic: hackathon working

---
+++ index.js
@@ -0,0 +1,44 @@
+const Octokit = require("@octokit/rest").plugin([
+  require("@octokit/plugin-retry"),
+  require("@octokit/plugin-throttling")
+]);
+
+const octokit = Octokit({
+  auth: "e0324842d4e4c6bc10b900112db9ffa861e231cf",
+  userAgent: "myApp v1.2.3",
+  timeZone: "Europe/Dublin",
+
+
+  throttle: {
+    onRateLimit: (retryAfter, options) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`
+      );
+
+      if (options.request.retryCount === 0) {
+        // only retries once
+        console.log(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      }
+    },
+    onAbuseLimit: (retryAfter, options) => {
+      // does not retry, only logs a warning
+      octokit.log.warn(
+        `Abuse detected for request ${options.method} ${options.url}`
+      );
+    }
+  }
+})
+
+async function getData() {
+
+  var data = await octokit.search.repos({
+    q: "hackathon + topic:hackathon",
+    per_page: 100
+  });
+
+  console.log(data);
+
+}
+
+getData();
