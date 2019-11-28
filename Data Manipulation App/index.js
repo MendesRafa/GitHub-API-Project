415220af6b26869dbc734e55030e7c0739f186dc
MendesRafa <rafaman089@outlook.com>
2019-11-28 10:03:01 +0000

    Data Manipulation App Completed and final data json written to filesystem to be used for test html

---
+++ Data Manipulation App/index.js
@@ -0,0 +1,60 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://mendesr:weLttJTuJjlrwRQF@githubproject-ncj49.mongodb.net/test?retryWrites=true&w=majority";
+const fs = require('fs');
+const util = require('util');
+
+async function getData() {
+        const client = await MongoClient.connect(uri, { useUnifiedTopology: true })
+          .catch(err => { console.log('Error occurred while connecting to MongoDB Atlas...\n',err); } );
+
+
+        if (client) {
+          console.log("connected\n")
+        }
+
+        try {
+          const db = client.db("Project");
+
+          let collection = db.collection("Repos");
+
+          let res = await collection.find({}).toArray();
+
+          console.log(res);
+
+          var justLangs = [];
+          for (i=0; i<res.length; i++) {
+            justLangs.push(res[i].languages);
+          }
+
+          var keys = [];
+
+          for (i=0; i<justLangs.length; i++) {
+            for(var k in justLangs[i]) keys.push(k);
+          }
+
+          var uniqueKeys = Array.from(new Set(keys));
+
+          var result = {};
+
+          for (i=0; i<uniqueKeys.length; i++) {
+            result[uniqueKeys[i]] = 0;
+          }
+
+          for (i=0; i<justLangs.length; i++){
+            for(var k in justLangs[i]) {
+              result[k] += justLangs[i][k];
+            }
+          }
+
+          fs.writeFileSync('./data.json', util.inspect(result) , 'utf-8');
+          
+        } catch(err) {
+          console.log(err);
+        } finally {
+          client.close();
+        }
+
+       
+}
+
+getData();
